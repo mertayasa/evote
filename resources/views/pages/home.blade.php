1bf7c7bdbcfeaf648c271fba8987c6887a01cde6
mertayasa <made.b4li@gmail.com>
2020-06-14 14:55:55 +0800

    Include bootstrap, make home controller and home views

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{url('dis/styles/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{url('dis/scripts/bootstrap.min.js')}}">
+  <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;600;700;800&display=swap" rel="stylesheet">
+  <title>Welcome Evote</title>
+</head>
+<body>
+  <h1>Hello</h1>
+</body>
+</html>
